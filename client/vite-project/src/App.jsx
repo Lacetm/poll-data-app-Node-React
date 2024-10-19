48456bb7516c36b8985da345020dfbbce0d00605
Lacet <lacetmarcosri@gmail.com>
2024-10-19 12:34:14 -0300

    initial

---
+++ client/vite-project/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+import{
+  useEffect,
+  useMemo,
+}from "react"
+import Bar from "./BarChart.jsx"
+import {io} from 'socket.io-client'
+
+function App() {
+  const socket = useMemo(() => 
+    io('http://localhost:5000'));
+
+  useEffect(() => {
+
+    socket.on('connect', () => 
+    console.log(socket.id))
+
+    socket.on('connect_error', () => {
+      setTimeout(() => 
+        socket.connect(), 5000)
+    })
+  return () =>{
+    socket.off('connect', () =>{
+      console.log("connected")
+    })
+    socket.off('disconnect', () => 
+      console.log('connected'));
+  };
+}, [])
+
+return (
+  <>
+    <h1>GFG REal-Time Polling App</h1>
+    <Bar socket={socket}/>
+
+  </>
+
+)
+
+}
+
+export default App
